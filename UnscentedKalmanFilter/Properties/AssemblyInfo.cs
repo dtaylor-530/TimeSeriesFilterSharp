3d08114121ec847857e1092d8d1c00474211f47e
dtaylor-530 <d.taylor530@gmail.com>
2022-07-08 09:12:10 +0100

    Updates AssemblyInfo.cs and csproj files

--- UnscentedKalmanFilter/Properties/AssemblyInfo.cs
+++ UnscentedKalmanFilter/Properties/AssemblyInfo.cs
@@ -1,4 +1,4 @@
-﻿using System.Reflection;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 
 // Information about this assembly is defined by the following attributes.
@@ -25,3 +25,5 @@ using System.Runtime.CompilerServices;
 //[assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyFile("")]
 
+
+[assembly: AssemblyInformationalVersion("1.0.0")]
