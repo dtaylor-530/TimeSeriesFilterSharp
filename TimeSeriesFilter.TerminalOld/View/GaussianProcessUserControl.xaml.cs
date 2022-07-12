b912a9da21db7b25a3822ef19e9719159ff6b2a0
dtaylor-530 <d.taylor530@gmail.com>
2022-07-12 22:36:28 +0100

    Improves the GaussianProcess code in the demo-app

--- TimeSeriesFilter.TerminalOld/View/GaussianProcessUserControl.xaml.cs
+++ TimeSeriesFilter.TerminalOld/View/GaussianProcessUserControl.xaml.cs
@@ -1,29 +1,6 @@
-﻿
-
-using FilterSharp.Common;
-using FilterSharp.ViewModel;
-using GaussianProcess;
-using GaussianProcess.Wrap;
-using MathNet.Numerics.Distributions;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FilterSharp.ViewModel;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-using System.Windows.Threading;
-
 
 namespace TimeSeriesFilterSharp.View
 {
@@ -32,143 +9,39 @@ namespace TimeSeriesFilterSharp.View
     /// </summary>
     public partial class GaussianProcessUserControl : UserControl
     {
-        private MasterGPViewModel mvm;
+        private GPViewModel2 mvm;
 
         public GaussianProcessUserControl()
         {
             InitializeComponent();
-            mvm = new MasterGPViewModel();
+            mvm = new GPViewModel2();
+            Secondary.DataContext = mvm;
             //this.DataContextChanged += GaussianProcessUserControl_DataContextChanged;
-            this.DataContext = mvm;
-          
+            //this.DataContext = mvm;
         }
-
-  
-        public class MasterGPViewModel:INotifyPropertyChanged
-        {
-
-            public GPViewModelStatic gpvm1 { get; set; } = new GPViewModelStatic();
-            public GPViewModel2 gpvm2 { get; set; } = new GPViewModel2();
-        
-
-            #region INotifyPropertyChanged Implementation
-            /// <summary>
-            /// Occurs when any properties are changed on this object.
-            /// </summary>
-            public event PropertyChangedEventHandler PropertyChanged;
-
-
-            /// <summary>
-            /// A helper method that raises the PropertyChanged event for a property.
-            /// </summary>
-            /// <param name="propertyNames">The names of the properties that changed.</param>
-            public virtual void NotifyChanged(params string[] propertyNames)
-            {
-                foreach (string name in propertyNames)
-                {
-                    PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
-                }
-            }
-
-
-            protected void RaisePropertyChanged([System.Runtime.CompilerServices.CallerMemberName] string caller = "")
-            {
-
-                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(caller));
-
-            }
-
- 
-            #endregion
-
-
-
-
-        }
-
-
-
-
 
         //******* Gaussian Process ********
 
-
-
         private async void ButtonSample_Click(object sender, RoutedEventArgs e)
         {
-
-
             for (int i = 0; i < 10; i++)
             {
-                mvm.gpvm2.Sample();
-                await Task.Run(() => System.Threading.Thread.Sleep(1000));
+                mvm.Sample();
+                //await Task.Run(() => System.Threading.Thread.Sleep(1000));
             }
-
-
         }
-
-
-
-
-
-        private void ButtonAdd_Click(object sender, RoutedEventArgs e)
-        {
-
-            mvm.gpvm1.Add();
-
-
-        }
-
-
-
-        private void ButtonRun_Click(object sender, RoutedEventArgs e)
-        {
-
-
-            mvm.gpvm1.Add(20);
-
-
-
-        }
-
-
-
-
 
         private void ButtonRunDynamic_Click(object sender, RoutedEventArgs e)
         {
-
             //public void Initialise(double a, double b)
             //{
             //    GaussianProcess = new GP2(new ExponentiatedQuadratic(), a, b);
 
             //}
 
-
-
             //return new AccordGenetic.Wrapper.TimeSeries2DOptimisation<IEnumerable<KeyValuePair<DateTime, double>>, IEnumerable<KeyValuePair<DateTime, Tuple<double, double>[]>>>
             //     ((ss) => (a, b) => TypeHelper.GetInstance<IFilterWrapper>(t, a, b).BatchRun(ss), e, ErrorHelper.GetErrorSum)
             //     .GetSubjectAsObservable();
-
-
-
         }
-
-
-
-
-        //private GaussianProcessWrapper Make()
-        //{
-
-
-        //}
-
-
-
-
-
     }
-
-
-   
 }
--- TimeSeriesFilter.TerminalOld/ViewModel/GaussianProccessViewModel.cs
+++ TimeSeriesFilter.TerminalOld/ViewModel/GaussianProccessViewModel.cs
@@ -1,151 +1,273 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GaussianProcess;
-using MathNet.Numerics.Distributions;
-using System.Collections.ObjectModel;
-using System.ComponentModel;
-
-using MathNet.Numerics.LinearAlgebra;
-
+﻿using FilterSharp.Common;
 using FilterSharp.Model;
+using GaussianProcess;
 using GaussianProcess.Wrap;
-
-using System.IO;
-using System.Windows;
-using System.Reflection;
-using System.Reactive.Linq;
-using System.Reactive.Concurrency;
-using System.Windows.Threading;
+using MathNet.Numerics.Distributions;
+using PropertyTools.Wpf;
 using ReactiveUI;
-using FilterSharp.Common;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace System.Runtime.CompilerServices
+{
+    internal static class IsExternalInit
+    { }
+}
 
 namespace FilterSharp.ViewModel
 {
-
-    
-    public class GPViewModelStatic : PredictionViewModel
+    public class GaussianProcessViewModel
     {
-        private Type kernel = typeof(GaussianProcess.RationalQuadratic);
+        public GaussianProcessViewModel()
+        {
+            ActionViewModel.Subscribe(ResultsViewModel);
+            ConfigurationViewModel.Subscribe(ResultsViewModel);
+            ResultsViewModel.Subscribe(ActionViewModel);
+        }
+
+        public GaussianProcessActionViewModel ActionViewModel { get; } = new();
+        public GaussianProcessResultsViewModel ResultsViewModel { get; } = new();
+        public GaussianProcessConfigurationViewModel ConfigurationViewModel { get; } = new();
+
+        public ICollection Children => new object[] { ActionViewModel, ConfigurationViewModel, ResultsViewModel };
+    }
+
+    public record GaussianProcessConfiguration(Type Kernel, double Length, double Noise);
+
+    public class GaussianProcessConfigurationViewModel : PredictionViewModel, IObservable<GaussianProcessConfiguration>
+    {
+        private Type kernel = typeof(RationalQuadratic);
         private double length = 3;
         private double noise = 3;
-        DateTime dt = new DateTime(2000, 1, 1);
-        DateTime odt = new DateTime(2000, 1, 1);
-        List<GP> gps = new List<GP>();
-        GaussianProcess.Wrap.GPMultiModelold gpmm;
 
+        public Dictionary<string, Type> Kernels { get; } = KernelHelper.LoadKernels().ToDictionary(a => a.GetDescription());
 
-
-        public Dictionary<string, Type> Kernels { get; set; }
-        
         public Type Kernel
         {
             get { return kernel; }
-            set { if (kernel != value) { kernel = value; KernelChanged(value, Length, Noise); } }
+            set { this.RaiseAndSetIfChanged(ref kernel, value); }
         }
-
 
         public double Length
         {
             get { return length; }
-            set { if (length != value) { this.RaiseAndSetIfChanged(ref length, value);  KernelChanged(Kernel, value, Noise); } }
+            set { this.RaiseAndSetIfChanged(ref length, value); }
         }
 
         public double Noise
         {
             get { return noise; }
-            set { if (noise != value) { this.RaiseAndSetIfChanged(ref noise, value); KernelChanged(Kernel, Length, value); } }
+            set { this.RaiseAndSetIfChanged(ref noise, value); }
         }
 
-
-
-
-        public GPViewModelStatic()
+        public IDisposable Subscribe(IObserver<GaussianProcessConfiguration> observer)
         {
-
-            Kernels = GaussianProcess.KernelHelper.LoadKernels().ToDictionary(_ => _.GetDescription());
-
-            KernelChanged(Kernels.First().Value, Length, Noise);
-
+            return this.WhenAnyValue(a => a.Kernel, a => a.Length, a => a.Noise)
+                .Select(a => new GaussianProcessConfiguration(a.Item1, a.Item2, a.Item3))
+                .Subscribe(a =>
+                {
+                    observer.OnNext(a);
+                });
         }
-
-
-
-        private void KernelChanged(Type type, double length, double noise)
-        {
-
-            gps.Clear();
-            gps.Add(new GP((IMatrixkernel)Activator.CreateInstance(type), length, noise));
-            gpmm = new GaussianProcess.Wrap.GPMultiModelold(gps.ToArray());
-
-            if (Measurements.Count > 0) Run();
-
-        }
-
-
-        public void Sample()
-        {
-
-            //Estimates.Clear();
-            //gpw.Sample().ForEach(_ => Estimates.Add(_));
-
-        }
-
-
-
-        public void Add(int cnt = 1)
-        {
-
-            foreach (var kvp in SignalGenerator.GetRandom(dt, cnt))
-            {
-                Measurements.Add(kvp);
-                dt = kvp.Key;
-            }
-
-            Run();
-        }
-
-
-
-        private void Run()
-        {
-
-            List<(double, double)> spoints = Measurements.Select(_ => ((double)(_.Key - odt).TotalSeconds, _.Value)).ToList();
-
-            var outgps = gpmm.AddTrainingPoints(spoints.Select(_ => _.Item1).ToArray(), spoints.Select(_ => _.Item2).ToArray());
-
-            var xx = outgps.Select(_ => _.mu
-              .Zip(_.sd95, (a, b) => new { Mu = a, Sigma = b })
-              .Zip(_.X, (c, d) =>
-               new Estimate(odt + TimeSpan.FromSeconds(d), c.Mu, c.Sigma)));
-            Estimates.Clear();
-            foreach (var __ in xx)
-                foreach (var ___ in __)
-                    Estimates.Add(___);
-
-        }
-
     }
 
+    public class GaussianProcessActionViewModel : ReactiveObject, IObservable<GaussianProcessAction>, IObserver<Exception?>
+    {
+        private readonly ReplaySubject<GaussianProcessAction> subject = new(1);
+        private string result;
 
+        public GaussianProcessActionViewModel()
+        {
+            Add = new DelegateCommand(() => subject.OnNext(GaussianProcessAction.Add));
+            Run = new DelegateCommand(() => subject.OnNext(GaussianProcessAction.Run));
+        }
 
+        public ICommand Run { get; }
+        public ICommand Add { get; }
 
+        public string Result
+        {
+            get { return result; }
+            set { this.RaiseAndSetIfChanged(ref result, value); }
+        }
 
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
 
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void OnNext(Exception? value)
+        {
+            if (value is { Message: var message })
+                Result = message;
+            else
+                Result = "SUCCESS";
+        }
+
+        public IDisposable Subscribe(IObserver<GaussianProcessAction> observer)
+        {
+            return subject.Subscribe(observer);
+        }
+    }
+
+    public enum GaussianProcessAction
+    {
+        Add, Run
+    }
+
+    public class GaussianProcessResultsViewModel : PredictionViewModel, IObserver<GaussianProcessConfiguration>, IObserver<GaussianProcessAction>, IObservable<Exception>
+    {
+        private DateTime dt = new(2000, 1, 1);
+        private DateTime odt = new(2000, 1, 1);
+        private GPMultiModelold multiModel;
+        //private GaussianProcessConfiguration value;
+
+        public DateTime Date
+        {
+            get { return dt; }
+            set { this.RaiseAndSetIfChanged(ref dt, value); }
+        }
+
+        public GPMultiModelold MultiModel
+        {
+            get { return multiModel; }
+            set { this.RaiseAndSetIfChanged(ref multiModel, value); }
+        }
+
+        public ObservableCollection<GP> Kernels { get; } = new();
+
+        //public void Sample()
+        //{
+        //    //Estimates.Clear();
+        //    //gpw.Sample().ForEach(_ => Estimates.Add(_));
+        //}
+
+        public Exception? Add(int cnt = 1)
+        {
+            try
+            {
+                foreach (var kvp in SignalGenerator.GetRandom(dt, cnt))
+                {
+                    Measurements.Add(kvp);
+                    Date = kvp.Key;
+                }
+
+                Run();
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+            return null;
+        }
+
+        private Exception? Run()
+        {
+            if (Measurements.Count == 0)
+            {
+            }
+
+            try
+            {
+                List<(double, double)> spoints = Measurements.Select(_ => ((double)(_.Key - odt).TotalSeconds, _.Value)).ToList();
+
+                var outgps = multiModel.AddTrainingPoints(spoints.Select(_ => _.Item1).ToArray(), spoints.Select(_ => _.Item2).ToArray());
+
+                var xx = outgps.SelectMany(a => a.mu
+                  .Zip(a.sd95, (a, b) => new { Mu = a, Sigma = b })
+                  .Zip(a.X, (c, d) =>
+                   new Estimate(odt + TimeSpan.FromSeconds(d), c.Mu, c.Sigma)));
+
+                AddToEstimates(xx);
+            }
+            catch (Exception ex)
+            {
+                return ex;
+            }
+            return null;
+        }
+
+        private async void AddToEstimates(IEnumerable<Estimate> estimates)
+        {
+            Estimates.Clear();
+            foreach (var est in estimates)
+            {
+                Estimates.Add(est);
+                await Task.Delay(5);
+            }
+        }
+
+        private ReplaySubject<Exception?> subject = new(1);
+
+        public void OnNext(GaussianProcessAction value)
+        {
+            switch (value)
+            {
+                case GaussianProcessAction.Add:
+                    subject.OnNext(Add(20));
+                    break;
+
+                case GaussianProcessAction.Run:
+                    subject.OnNext(Run());
+                    break;
+
+                default:
+                    throw new Exception("fgdsf3 dfgfgs");
+            }
+        }
+
+        public void OnNext(GaussianProcessConfiguration value)
+        {
+            KernelChanged(value.Kernel, value.Length, value.Noise);
+
+            void KernelChanged(Type type, double length, double noise)
+            {
+                Kernels.Clear();
+                Kernels.Add(new GP((IMatrixkernel)Activator.CreateInstance(type), length, noise));
+                multiModel = new GPMultiModelold(Kernels.ToArray());
+
+                if (Measurements.Count > 0)
+                    Run();
+            }
+        }
+
+        public void OnError(Exception error)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void OnCompleted()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDisposable Subscribe(IObserver<Exception> observer)
+        {
+            return subject.Subscribe(observer);
+        }
+    }
 
     public class GPViewModel2 : PredictionViewModel
     {
-
         [PropertyTools.DataAnnotations.Browsable(false)]
         public Wrapper gpw { get; private set; }
 
-        double[] axpts;
+        private double[] axpts;
 
-        DateTime dt = new DateTime(2000, 1, 1);
-        Random rand = new Random();
+        private DateTime dt = new DateTime(2000, 1, 1);
+        private Random rand = new Random();
 
         public GPViewModel2(List<Tuple<double, double>> points = null, double scale = 100, IKernel kernel = null)
         {
@@ -162,22 +284,8 @@ namespace FilterSharp.ViewModel
 
         public void Sample()
         {
-
             Estimates.Clear();
             gpw.Sample().ForEach(_ => Estimates.Add(_));
-
         }
-
-
     }
-
-
-
-
-
-
-
 }
-
-
-
